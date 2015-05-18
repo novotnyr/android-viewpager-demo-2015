55f2e7da3284df4906f5091fc84688a81dacb90a
novotnyr <robert.novotny@upjs.sk>
2015-05-18 21:00:46 +0200

    add tabs and fragment changing into main activity

--- app/src/main/java/sk/upjs/ics/android/slovensko/MainActivity.java
+++ app/src/main/java/sk/upjs/ics/android/slovensko/MainActivity.java
@@ -1,16 +1,72 @@
 package sk.upjs.ics.android.slovensko;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
+import static android.support.v7.app.ActionBar.NAVIGATION_MODE_TABS;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setNavigationMode(NAVIGATION_MODE_TABS);
+        configureRegionListTab();
+        configureCountyListTab();
     }
 
+    private void configureRegionListTab() {
+        ActionBar actionBar = getSupportActionBar();
+
+        ActionBar.Tab tab = actionBar.newTab();
+        tab.setText("Kraje");
+        tab.setTabListener(this);
+        tab.setTag(RegionListFragment.class.getName());
+
+        actionBar.addTab(tab);
+    }
+
+
+    private void configureCountyListTab() {
+        ActionBar actionBar = getSupportActionBar();
+
+        ActionBar.Tab tab = actionBar.newTab();
+        tab.setText("Okresy");
+        tab.setTabListener(this);
+        tab.setTag(CountyListFragment.class.getName());
+
+        actionBar.addTab(tab);
+    }
+
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        String tag = (String) tab.getTag();
+        String fragmentClassName = tag;
+
+        Fragment selectedFragment = getSupportFragmentManager().findFragmentByTag(tag);
+        if (selectedFragment == null) {
+            selectedFragment = Fragment.instantiate(this, fragmentClassName);
+            ft.add(android.R.id.content, selectedFragment, tag);
+        } else {
+            ft.attach(selectedFragment);
+        }
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        String tag = (String) tab.getTag();
+        Fragment selectedFragment = getSupportFragmentManager().findFragmentByTag(tag);
+        if (selectedFragment != null) {
+            ft.detach(selectedFragment);
+        }
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
 }
